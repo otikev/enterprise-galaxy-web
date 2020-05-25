0c1643bcca9439996d89720f6cb0bb03fcbe5b6a
Kevin Otieno <k.otieno@kokonetworks.com>
2020-05-25 15:00:33 +0300

    generated enterprises controller

---
+++ app/controllers/enterprise_controller.rb
@@ -0,0 +1,2 @@
+class EnterpriseController < ApplicationController
+end
---
+++ test/controllers/enterprise_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EnterpriseControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
