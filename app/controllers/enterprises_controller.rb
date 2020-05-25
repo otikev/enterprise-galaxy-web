87b0240c76a8d66df3cbc73988ec50831a54be7a
Kevin Otieno <k.otieno@kokonetworks.com>
2020-05-25 15:52:13 +0300

    Ability to create an enterprise (WIP)

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -6,4 +6,17 @@ class EnterprisesController < ApplicationController
   def new
 
   end
+
+  def create
+    @enterprise = Enterprise.new(enterprise_params)
+    @enterprise.save
+    redirect_to @enterprise
+  end
+
+  private
+  def enterprise_params
+    params.require(:enterprise).permit(:business_name,:contact_email,:start_of_operations_date,
+                                       :registration_date,:postal_address,:postal_code,:country,
+                                       :city,:office_number,:office_phone)
+  end
 end
---
+++ db/schema.rb
@@ -0,0 +1,33 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_05_25_122921) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "enterprises", force: :cascade do |t|
+    t.string "business_name"
+    t.string "contact_email"
+    t.date "start_of_operations_date"
+    t.date "registration_date"
+    t.string "postal_address"
+    t.string "postal_code"
+    t.string "country"
+    t.string "city"
+    t.string "office_number"
+    t.string "office_phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
