ccc9c90089e67fe4f80f389c479f48c038ef1e0d
Kevin Otieno <k.otieno@kokonetworks.com>
2020-05-25 15:12:04 +0300

    remove 'enterprise' scaffolded files

--- app/controllers/enterprise_controller.rb
+++
@@ -1,2 +0,0 @@
-class EnterpriseController < ApplicationController
-end
--- test/controllers/enterprise_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class EnterpriseControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
