a7a39725ab18a7a021f31b097f4c303eb1c93793
Kevin Otieno <k.otieno@kokonetworks.com>
2020-05-25 17:32:43 +0300

    Open social links in new browser tab + add collection lists to the enterprise signup page

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -8,6 +8,7 @@ class EnterprisesController < ApplicationController
   end
 
   def create
+    @business_forms = BusinessForm.all
     @enterprise = Enterprise.new(enterprise_params)
     @enterprise.save
     redirect_to @enterprise
@@ -17,6 +18,6 @@ class EnterprisesController < ApplicationController
   def enterprise_params
     params.require(:enterprise).permit(:business_name,:contact_email,:start_of_operations_date,
                                        :registration_date,:postal_address,:postal_code,:country,
-                                       :city,:office_number,:office_phone)
+                                       :city,:office_number,:office_phone,:business_form_id,:broad_sector_name_id)
   end
 end
