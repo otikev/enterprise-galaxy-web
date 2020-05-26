986a562242a9f830476838e259c5cbdacbb52015
Andrew Owino <andystudy4@gmail.com>
2020-05-26 17:12:10 +0300

    Activate enterprise upon clicking activation link

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2020_05_26_123641) do
   end
 
   create_table "enterprises", force: :cascade do |t|
+    t.bigint "business_form_id"
+    t.bigint "broad_sector_name_id"
     t.string "business_name"
     t.string "contact_email"
     t.date "start_of_operations_date"
@@ -43,6 +45,8 @@ ActiveRecord::Schema.define(version: 2020_05_26_123641) do
     t.string "activation_digest"
     t.boolean "activated"
     t.datetime "activated_at"
+    t.index ["broad_sector_name_id"], name: "index_enterprises_on_broad_sector_name_id"
+    t.index ["business_form_id"], name: "index_enterprises_on_business_form_id"
   end
 
 end
