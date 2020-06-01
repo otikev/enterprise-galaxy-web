6e70a453bce6a1b60cbe740051f0a87b6dac2379
Kevin Otieno <k.otieno@kokonetworks.com>
2020-06-01 14:23:25 +0300

    setting up the theme

--- Gemfile
+++ Gemfile
@@ -18,6 +18,9 @@ gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
 
+gem 'jquery-rails'
+gem 'bootstrap-sass'
+
 gem 'country-select'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :advisers
   match '/countdown', to: 'site#countdown', via: 'get'
   match '/signup', to: 'enterprises#signup', via: 'get'
+  match '/theme', to: 'enterprises#theme', via: 'get'
   resources :account_activations, only: [:edit]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
