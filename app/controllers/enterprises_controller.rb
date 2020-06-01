eb91ea00fba361fafddb95fee0d4d91e7ee84110
Kevin Otieno <k.otieno@kokonetworks.com>
2020-06-01 10:44:45 +0300

    Signup stuff

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -7,14 +7,21 @@ class EnterprisesController < ApplicationController
 
   end
 
+  def success
+
+  end
+
+  def show
+    @enterprise = Enterprise.where(:id => params[:id]).first
+  end
+
   def create
-    @business_forms = BusinessForm.all
     @enterprise = Enterprise.new(enterprise_params)
     
     if @enterprise.save
       EnterpriseMailer.account_activation(@enterprise).deliver_now
       flash[:info] = "Please check your email to activate your account."    
-      redirect_to @enterprise
+      redirect_to success
     else
       render 'new'
     end
@@ -24,6 +31,7 @@ class EnterprisesController < ApplicationController
   def enterprise_params
     params.require(:enterprise).permit(:business_name,:contact_email,:start_of_operations_date,
                                        :registration_date,:postal_address,:postal_code,:country,
-                                       :city,:office_number,:office_phone,:business_form_id,:broad_sector_name_id)
+                                       :city,:office_number,:office_phone,:business_form_id,:broad_sector_name_id,
+                                       :registration_date,:start_of_operations_date)
   end
 end
